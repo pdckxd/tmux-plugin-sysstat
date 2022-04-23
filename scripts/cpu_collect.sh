f639eef02c065f57cf9250b31f8de6a5483cd509
ravenxrz <zhang.xingrui@foxmail.com>
2022-04-23 19:32:08 +0800

    fix cpu collect interval and sample count to 1

--- scripts/cpu_collect.sh
+++ scripts/cpu_collect.sh
@@ -8,8 +8,8 @@ set -e
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
-refresh_interval=$(get_tmux_option "status-interval" "5")
-samples_count="60"
+refresh_interval=1
+samples_count=1
 cpu_metric_file="$(get_tmux_option "@sysstat_cpu_tmp_dir" "/dev/null")/cpu_collect.metric"
 
 get_cpu_usage() {
@@ -21,14 +21,6 @@ get_cpu_usage() {
       top -l "$samples_count" -s "$refresh_interval" -n 0 \
         | sed -u -nr '/CPU usage/s/.*,[[:space:]]*([0-9]+[.,][0-9]*)%[[:space:]]*idle.*/\1/p' \
         | stdbuf -o0 awk '{ print 100-$0 }'
-    fi
-  elif ! command_exists "vmstat"; then
-    if is_freebsd; then
-      vmstat -n "$refresh_interval" -c "$samples_count" \
-        | stdbuf -o0 awk 'NR>2 {print 100-$(NF-0)}'
-    else
-      vmstat -n "$refresh_interval" "$samples_count" \
-        | stdbuf -o0 awk 'NR>2 {print 100-$(NF-2)}'
     fi
   else
     if is_freebsd; then
